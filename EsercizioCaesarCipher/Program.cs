5f4c70e0c30d61cb4e9a11586330649d86006906
Gianmarco Montagna <g.montagna@arsera.it>
2024-02-15 17:53:28 +0100

    aggiunto esercizio caesar cipher

---
+++ EsercizioCaesarCipher/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace EsercizioCaesarCipher;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        string plainText = "Hello World!";
+        int key = 200;
+
+        string cipherText = Crypt(plainText, key);
+        Console.WriteLine(cipherText);
+
+        string decryptedText = Decrypt(cipherText, key);
+        Console.WriteLine(decryptedText);
+
+    }
+
+    public static string Crypt(string plainText, int key)
+    {
+        char[] chars = plainText.ToCharArray();
+        char[] ciphredChars = new char[chars.Length];
+
+        for (int i = 0; i < chars.Length; i++)
+        {
+            char letter = chars[i];
+
+            ciphredChars[i] = ProcessChar(letter, key);
+        }
+
+        return new string(ciphredChars);
+    }
+
+    public static string Decrypt(string cipherText, int key)
+    {
+        char[] chars = cipherText.ToCharArray();
+        char[] plainChars = new char[chars.Length];
+
+        for (int i = 0; i < chars.Length; i++)
+        {
+            char letter = chars[i];
+
+            plainChars[i] = ProcessChar(letter, -key);
+        }
+
+        return new string(plainChars);
+    }
+
+    public static char ProcessChar(char letter, int key)
+    {
+        if (!char.IsLetter(letter))
+        {
+            return letter;
+        }
+
+        int letterValue = letter;
+
+        int offset = GetOffset(letter);
+
+        int positionInAlphabet = letterValue - offset;
+        int valueWithKey = ModuloCheFunziona(positionInAlphabet + key, 26);
+        int cipherValue = valueWithKey + offset;
+
+        return (char)cipherValue;
+    }
+
+    public static int ModuloCheFunziona(int a, int b)
+    {
+        return (a % b + b) % b;
+    }
+
+    public static int GetOffset(char letter)
+    {
+        if (char.IsUpper(letter))
+        {
+            return 'A';
+        }
+        else
+        {
+            return 'a';
+        }
+    }
+}
---
+++ EsercizioCaesarCipher2/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace EsercizioCaesarCipher2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string plainText = "Hello World!";
+
+        int key = 200;
+
+        string cipherText = Crypt(plainText, key);
+        Console.WriteLine("cipher text: " + cipherText);
+
+        string decryptedText = Decrypt(cipherText, key);
+        Console.WriteLine("decrypted text: " + decryptedText);
+    }
+
+
+    public static char ProcessChar(char letter, int key)
+    {
+        //Console.WriteLine("letter: " + letter);
+
+        int letterInt = letter;
+
+        //Console.WriteLine("letterInt: " + letterInt);
+
+        int resultInt = letterInt + key;
+
+        //Console.WriteLine("resultInt: " + resultInt);
+
+        char resultChar = (char)resultInt;
+
+        //Console.WriteLine("resultChar: " + resultChar);
+
+        return resultChar;
+    }
+
+    public static string Crypt(string plainText, int key)
+    {
+        return ProcessString(plainText, key);
+    }
+    public static string Decrypt(string cipherText, int key)
+    {
+        return ProcessString(cipherText, -key);
+    }
+
+    public static string ProcessString(string input, int key)
+    {
+        char[] chars = input.ToArray();
+        char[] outputChars = new char[chars.Length];
+
+        // faccio il calcolo
+        for (int i = 0; i < chars.Length; i++)
+        {
+            char letter = chars[i];
+
+            outputChars[i] = ProcessChar(letter, -key);
+        }
+
+        return new string(outputChars);
+    }
+
+}
